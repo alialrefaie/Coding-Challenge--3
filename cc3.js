0bab31c800b55cccd48097cd4388fc87afcea6ae
Ali Alrefaie <alrefaiea6@gmail.com>
2024-05-21 22:55:38 -0400

    Continued fixes of test accelerate and brake methods on car instances

--- cc3.js
+++ cc3.js
@@ -1,4 +1,3 @@
-U72955273
 // Car Class
 
 class Car {
@@ -13,33 +12,30 @@ class Car {
 
 accelerate() {
     this.speed += 10;
-    console.log(`${this.make} is going at ${this.speed} km/h`);
-    }
+    console.log(`${this.make} is accelerating. New speed: ${this.speed} km/h`);
 }
+
 
 // Brake method
 
 brake() {
     this.speed -= 5;
-    console.log(`${this.make} is going at ${this.speed} km/h`);
+    console.log(`${this.make} is braking. New speed: ${this.speed} km/h`);
     }
+}
 
-// Create and Test Car Objectives
+//Create and Test Car Objectives
+const car1 = new Car('BMW',120);
+const car2 = new Car('Mercedes',95);
 
-// Car 1: BMW starting at 120 km/h
-let car1 = new Car('BMW' , 120);
+console.log("Testing Car 1:");
+car1.accelerate(); // Should log: BMW is accelerating. New speed: 130 km/h
+car1.accelerate(); // Should log: BMW is accelerating. New speed: 140 km/h
+car1.brake(); // Should log: BMW is braking. New speed: 135 km/h
+car1.brake(); // Should log: Bmw is braking. New speed: 130 km/h
 
-//Car 2: Mercedes starting at 95 km/h
-let car2 = new Car('Mercedes' , 95);
-
-// Test accelerate and brake methods on car 1
-car1.accelerate(); // BMW is going at 130 km/h
-car1.accelerate(); // BMW is going at 140 km/h
-car1.brake() // BMW is going at 135 km/h
-car1.brake() // BMW is going at 130 km/h
-
-// Test accelerate and brake methods on car 2
-car2.accelerate(); // Mercedes is goign at 105 km/h
-car2.accelerate(); // Mercedes is going at 115 km/h
-car2.brake(); // Mercedes is going at 110 km/h
-car2.brake(); // Mercedes is going at 105 km/h
+console.log("Testing Car 2:");
+car2.accelerate(); // Should log: Mercedes is accelerating. New speed: 105 km/h
+car2.accelerate(); // Should log: Mercedes is accelerating. New speed: 115 km/h
+car2.brake(); // Should log: Mercedes is braking. New speed: 110 km/h
+car2.brake(); // Should log: Mercedes is braking. New speed: 105 km/h
