a33971eee59bb14bc96e1cbefa79047cc1a348c6
Ali Alrefaie <alrefaiea6@gmail.com>
2024-05-21 22:23:06 -0400

    Fixed test accelerate and brake methods on car instances

--- cc3.js
+++ cc3.js
@@ -1,4 +1,6 @@
+U72955273
 // Car Class
+
 class Car {
     constructor(make, speed) {
         this.make = make;
@@ -8,6 +10,7 @@ class Car {
 
 
 // Accelerate method
+
 accelerate() {
     this.speed += 10;
     console.log(`${this.make} is going at ${this.speed} km/h`);
@@ -15,6 +18,7 @@ accelerate() {
 }
 
 // Brake method
+
 brake() {
     this.speed -= 5;
     console.log(`${this.make} is going at ${this.speed} km/h`);
@@ -29,13 +33,13 @@ let car1 = new Car('BMW' , 120);
 let car2 = new Car('Mercedes' , 95);
 
 // Test accelerate and brake methods on car 1
-car1accelerate; // BMW is going at 130 km/h
-car1accelerate; // BMW is going at 140 km/h
-car1brake // BMW is going at 135 km/h
-car1brake // BMW is going at 130 km/h
+car1.accelerate(); // BMW is going at 130 km/h
+car1.accelerate(); // BMW is going at 140 km/h
+car1.brake() // BMW is going at 135 km/h
+car1.brake() // BMW is going at 130 km/h
 
 // Test accelerate and brake methods on car 2
-car2accelerate; // Mercedes is goign at 105 km/h
-car2accelerate; // Mercedes is going at 115 km/h
-car2brake; // Mercedes is going at 110 km/h
-car2brake; // Mercedes is going at 105 km/h
+car2.accelerate(); // Mercedes is goign at 105 km/h
+car2.accelerate(); // Mercedes is going at 115 km/h
+car2.brake(); // Mercedes is going at 110 km/h
+car2.brake(); // Mercedes is going at 105 km/h
