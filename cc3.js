2eea93d4c46d698f8057b2f168159dbb940b0a63
Ali Alrefaie <alrefaiea6@gmail.com>
2024-05-21 21:34:26 -0400

    Test accelerate and brake methods on Car instances

--- cc3.js
+++ cc3.js
@@ -7,15 +7,35 @@ class Car {
 
 
 
-//Accelerate method
+// Accelerate method
 accelerate() {
     this.speed += 10;
     console.log(`${this.make} is going at ${this.speed} km/h`);
     }
 }
 
-//Brake method
+// Brake method
 brake() {
     this.speed -= 5;
     console.log(`${this.make} is going at ${this.speed} km/h`);
     }
+
+// Create and Test Car Objectives
+
+// Car 1: BMW starting at 120 km/h
+let car1 = new Car('BMW' , 120);
+
+//Car 2: Mercedes starting at 95 km/h
+let car2 = new Car('Mercedes' , 95);
+
+// Test accelerate and brake methods on car 1
+car1accelerate; // BMW is going at 130 km/h
+car1accelerate; // BMW is going at 140 km/h
+car1brake // BMW is going at 135 km/h
+car1brake // BMW is going at 130 km/h
+
+// Test accelerate and brake methods on car 2
+car2accelerate; // Mercedes is goign at 105 km/h
+car2accelerate; // Mercedes is going at 115 km/h
+car2brake; // Mercedes is going at 110 km/h
+car2brake; // Mercedes is going at 105 km/h
